969592e6edaa79a2a3ab5e84287b51eda20653f2
Paulac25 <condepaula472@gmail.com>
2025-09-18 23:24:14 -0500

    Update modificacion

--- Prueba/Prueba/resources/views/cliente/reservaciones/index.blade.php
+++ Prueba/Prueba/resources/views/cliente/reservaciones/index.blade.php
@@ -6,11 +6,12 @@
     <h1 class="h4">Reservaciones</h1>
     <a href="{{ route('reservaciones.create') }}" class="btn btn-primary">Nuevo</a>
 </div>
+
 <table class="table table-striped">
     <thead>
         <tr>
             <th>ID del usuario</th>
-            <th>Nombre y Cedula</th>
+            <th>Nombre</th>
             <th>Cantidad personas</th>
             <th>Cantidad de mesas</th>
             <th>Fecha de la reserva</th>
@@ -19,25 +20,24 @@
         </tr>
     </thead>
     <tbody>
-    @foreach ($reservaciones as $reservacion)
-    <tr>
-        <td>{{ $reservacion->id }}</td>
-        <td>{{ $reservacion->user?->name ?? 'Usuario no asignado' }}</td>
-
-        <td>{{ $reservacion->cantidad_personas }}</td>
-        <td>{{ $reservacion->cantidad_mesas }}</td>
-        <td>{{ $reservacion->fecha_reservacion }}</td>
-        <td>{{ $reservacion->ocasion }}</td>
-        <td>
-            <a href="{{ route('reservaciones.show',$reservacion)}}" class="btn btn-info">Ver</a>
-            <a href="{{ route('reservaciones.edit',$reservacion) }}" class="btn btn-warning">Editar</a>
-            <form action="{{ route('reservaciones.destroy',$reservacion) }}" method="post" class="d-inline">
-        @csrf @method('DELETE')
-        <button class="btn btn-sm btn-danger" onclick="return confirm('Eliminar producto')">Eliminar</button>    
-        </form>
-        </td>
-</tr>
-    
+        @foreach ($reservaciones as $reservacion)
+        <tr>
+            <td>{{ $reservacion->id }}</td>
+            <td>{{ $reservacion->user?->name ?? 'Usuario no asignado' }}</td>
+            <td>{{ $reservacion->cantidad_personas }}</td>
+            <td>{{ $reservacion->cantidad_mesas }}</td>
+            <td>{{ $reservacion->fecha_reservacion }}</td>
+            <td>{{ $reservacion->ocasion }}</td>
+            <td>
+                <a href="{{ route('reservaciones.show',$reservacion)}}" class="btn btn-info">Ver</a>
+                <a href="{{ route('reservaciones.edit',$reservacion) }}" class="btn btn-warning">Editar</a>
+                <form action="{{ route('reservaciones.destroy',$reservacion) }}" method="post" class="d-inline">
+                    @csrf @method('DELETE')
+                    <button class="btn btn-sm btn-danger" onclick="return confirm('Eliminar producto')">Eliminar</button>    
+                </form>
+            </td>
+        </tr>
+        @endforeach
     </tbody>
-    @endforeach
+</table>
 @endsection
